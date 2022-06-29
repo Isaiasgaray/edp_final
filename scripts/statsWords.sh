e83dc7a6cccc8e9a9fb21585ace2887890d6fa31
Isaías Garay Gorta <isaiasgaray2014@gmail.com>
2022-06-28 23:28:59 -0300

    Agrega el ejercicio 1 y el archivo de prueba "zen.txt"

---
+++ scripts/statsWords.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+[ "$#" -ne 1 ] && exit 1
+
+LONGEST=""
+SHORTEST=""
+
+while read -r line; do
+  for word in $line; do
+
+    # Si la variable SHORTEST está vacía se o si 
+    # la variable word tiene menos caracteres que SHORTEST
+    # le asigna el valor de word a SHORTEST
+    if [ -z "$SHORTEST" ] || [ "${#word}" -lt "${#SHORTEST}" ]; then
+      SHORTEST="$word"
+    fi
+
+    # Si la variable word tiene más caracteres que
+    # LONGEST se le asigna el valor de word a LONGEST
+    if [ "${#word}" -gt "${#LONGEST}" ]; then
+      LONGEST="$word"
+    fi
+  done
+done < "${1}"
+
+# Se divide la cantidad de caracteres por la cantidad de palabras
+# del archivo para obtener el promedio de longitud de palabras.
+PROMEDIO=$(echo "scale = 2; $(wc -m < $1) / $(wc -w < $1)" | bc)
+
+# Se imprimen en pantalla los resultados.
+echo "Palabra más larga: $LONGEST"
+echo "Palabra más cort: $SHORTEST"
+echo "Promedio de longitud: $PROMEDIO"
