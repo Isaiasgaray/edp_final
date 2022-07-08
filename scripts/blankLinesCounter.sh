e2fb5921263c4ccd5a4feed41f35ef01bef7075a
Isaías Garay Gorta <isaiasgaray2014@gmail.com>
2022-07-08 16:33:30 -0300

    Agrega comentarios al script.

--- scripts/blankLinesCounter.sh
+++ scripts/blankLinesCounter.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-[ $# -ne 1 ] && exit 1
+source lib.sh
+test_file $# $1
 
-grep -c "^$" "${1}"
+# Detecta líneas vacías mediante una expresión regular.
+# Luego con una opción de grep las cuenta.
+grep -c "^$" ${1}
