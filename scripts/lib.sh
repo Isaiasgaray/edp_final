07243d0b4d83a7fab5c6d9f66abcc60f47b857b2
Isaías Garay Gorta <isaiasgaraygorta@gmail.com>
2022-07-07 21:14:04 -0300

    Agrega script de funciones genéricas.

---
+++ scripts/lib.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function err {
+  # Envia mensajes al stderr.
+  echo "$1" 1>&2
+}
+
+function no_file {
+  # Si no hay archivos de entrada devuelve 1.
+  ! [ $1 -eq 0 ]
+}
+
+function no_file_err {
+  # Mensaje de error genérico para funciones
+  # que necesita un archivo como input.
+  err "Usage: ./$0 file.txt"
+  exit 1
+}
