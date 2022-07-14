ae3a7d09842eed6c20bb9d7919aaa916ca90efb2
Claudia Evangelina Leguiza <107651894+claudialeguiza@users.noreply.github.com>
2022-07-14 16:55:18 -0300

    Ejercicio 9
    Creación de script

---
+++ scripts/palindromeDetection.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# source lib.sh
+# test_file $# $1
+
+# Función auxiliar para detectar palíndromos.
+function es_palindromo {
+  [ "${1,,}" = $(echo ${1,,} | rev) ]
+}
+
+# Filtra palabras que tenga tres caractres o más.
+FILE=$(grep -oE "\b[a-zA-Z]{3,}\b" ${1})
+
+# Si hay algún caracter con tilde en el archivo, filtra todas las tildes.
+if echo $FILE | grep -iq "[áéíóú]"; then
+  FILE=$(echo $FILE | sed 'y/ÁÉÍÓÚáéíóú/AEIOUaeiou/')
+fi
+
+# Itera sobre cada palabra, si la palabra es un
+# palíndromo la imprime.
+for word in $(echo $FILE); do
+  if es_palindromo $word; then
+    echo $word
+  fi
+done
